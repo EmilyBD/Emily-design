87c9f8b8518d293011e1c7ad9c2480e1fb7eea1c
sheldon5 <sheldonhit@gmail.com>
2024-09-20 14:44:58 +0800

    首页

--- src/views/Catalogue.vue
+++ src/views/Catalogue.vue
@@ -3,14 +3,17 @@
         <div class="catalogue-page__header">
             <div class="catalogue-title">Emily's Design</div>
             <div class="catalogue-subtitle">种一棵树最好的时间是十年前，其次是现在</div>
+            <img src="../assets/images/catalogue/arrow_red.svg" alt="" class="arrow-red">
         </div>
         <div class="catalogue-content">
             <section class="catelogue-block" v-for="(cata, index) in catalogues" :key="cata.name">
+                <img src="../assets/images/catalogue/arrow_grey.svg" alt="" class="arrow-grey">
                 <div class="img-container">
                     <img class="catalogue-thumbnail" :src="cata.pic" />
                 </div>
                 <p class="catalogue-name">{{ cata.name }}</p>
                 <p class="catalogue-no">{{ index }}</p>
+                <span class="bottom-line"></span>
             </section>
         </div>
     </div>
@@ -68,6 +71,7 @@ const catalogues = ref([
         padding-bottom: 2rem;
         margin-bottom: 2rem;
         background-color: #fff;
+        position: relative;
 
         .catalogue-title {
             font-size: $page-title-fontsize;
@@ -77,6 +81,12 @@ const catalogues = ref([
         .catalogue-subtitle {
             font-size: $page-subtitle-fontsize;
         }
+
+        .arrow-red {
+            position: absolute;
+            bottom: -14px;
+            left: calc(50% - 14px);
+        }
     }
 
     .catalogue-content {
@@ -85,11 +95,12 @@ const catalogues = ref([
         flex-direction: row;
         justify-content: space-between;
         min-width: 100%;
+        margin-top: 2rem;
+        padding-bottom: 10rem;
 
         .catelogue-block {
             flex: 1;
             width: 25%;
-            height: 480px;
             display: flex;
             justify-content: center;
             flex-direction: column;
@@ -98,15 +109,17 @@ const catalogues = ref([
             border: 1px solid rgba(223, 227, 235, 1);
             padding: 1rem;
             cursor: pointer;
+            margin-bottom: 10rem;
+            position: relative;
 
             &:not(:last-child) {
                 margin-right: 2rem;
             }
 
             &:hover {
-                height: 580px;
                 border: 2px solid rgba(203, 214, 226, 1);
                 box-shadow: 0px 2px 4px 1px rgba(124, 152, 182, 0.15);
+                margin-bottom: 0;
 
                 .img-container {
                     .catalogue-thumbnail {
@@ -115,27 +128,42 @@ const catalogues = ref([
                         transform: translate(0, 0);
                     }
                 }
+
+                .arrow-grey {
+                    display: none;
+                }
             }
 
             .img-container {
                 width: 100%;
                 flex: 1;
+                text-align: center;
 
                 .catalogue-thumbnail {
-                    width: 50%;
-                    height: 50%;
-                    transform: translate(50%, 50%);
+                    width: 14rem;
+                    height: 14rem;
+                    transform: translateY(4rem);
                 }
             }
 
             .catalogue-name {
                 font-size: 2.4rem;
                 color: #33475B;
+                margin-top: 12rem;
             }
 
             .catalogue-no {
                 font-size: 1.8rem;
                 color: $light-font-color;
+                margin-top: 4rem;
+            }
+
+            .bottom-line {
+                display: inline-block;
+                width: 8rem;
+                border-top: 6px solid #DFE3EB;
+                position: absolute;
+                bottom: 0;
             }
         }
     }
