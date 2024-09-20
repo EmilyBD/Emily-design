ff8b1ac24d2797161da2862292791455a1b0b841
sheldon5 <sheldonhit@gmail.com>
2024-09-20 14:55:48 +0800

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: sheldon5 sheldonhit@gmail.com
+ * @Date: 2024-09-20 14:19:18
+ * @LastEditors: sheldon5 sheldonhit@gmail.com
+ * @LastEditTime: 2024-09-20 14:54:44
+ * @FilePath: \emily-design\vite.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -15,5 +23,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'docs'
-  }
+  },
+  base: './'
 });
